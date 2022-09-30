e1f8664adb6fc71917c0de1a4779b3053457e716
collinblank <cablank25@gmail.com>
2022-09-30 18:53:34 -0400

    updated for heroku

---
+++ config/config.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+module.exports = 
+{
+  "development": {
+    "username": "bzgzflsmafpggh",
+    "password": "a6e93d53aa13734ab6d75490d100c81ae18463524abdeb11b1e5ce891aa3dc22",
+    "database": "d7ckka69flggqj",
+    "host": "ec2-52-207-90-231.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "dialectOptions":{   
+      "ssl":{
+        "require":true,
+        "rejectUnauthorized":false
+      }
+    }
+  },
+  "test": {
+    "username": "bzgzflsmafpggh",
+    "password": "a6e93d53aa13734ab6d75490d100c81ae18463524abdeb11b1e5ce891aa3dc22",
+    "database": "d7ckka69flggqj-test",
+    "host": "ec2-52-207-90-231.compute-1.amazonaws.com",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "bzgzflsmafpggh",
+    "password": "a6e93d53aa13734ab6d75490d100c81ae18463524abdeb11b1e5ce891aa3dc22",
+    "database": "d7ckka69flggqj-test",
+    "host": "ec2-52-207-90-231.compute-1.amazonaws.com",
+    "dialect": "postgres"
+  }
+}
