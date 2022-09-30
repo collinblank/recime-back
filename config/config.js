a317b84628e9922dc29bac0dde4db8af060421a7
collinblank <cablank25@gmail.com>
2022-09-30 19:48:17 -0400

    heroku fix

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports =
     "dialect": "postgres",
     "dialectOptions":{   
       "ssl":{
-        "require": false,
+        "require": true,
         "rejectUnauthorized": false
       }
     }
