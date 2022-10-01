baa8bb3df9fd386954f9a1417479e0d57dca24b9
collinblank <cablank25@gmail.com>
2022-09-30 20:56:33 -0400

    fix heroku

--- config/config.js
+++ config/config.js
@@ -23,10 +23,10 @@ module.exports =
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": "bzgzflsmafpggh",
+    "password": "a6e93d53aa13734ab6d75490d100c81ae18463524abdeb11b1e5ce891aa3dc22",
+    "database": "d7ckka69flggqj",
+    "host": "ec2-52-207-90-231.compute-1.amazonaws.com",
     "dialect": "postgres",
     "dialectOptions":{   
       "ssl":{
