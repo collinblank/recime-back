1d340b1892b2371867dfae4c187ba3d53c90b5a1
collinblank <cablank25@gmail.com>
2022-10-06 21:10:31 -0400

    fix heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-// const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session')
 // const defineCurrentUser = require('./middleware/defineCurrentUser')
 
 // CONFIGURATION / MIDDLEWARE
