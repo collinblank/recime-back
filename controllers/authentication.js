13f3d969749a9e15a5f2bbbdc474b50fe4c8ed01
collinblank <cablank25@gmail.com>
2022-10-07 18:41:07 -0400

    updating for login form

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -11,33 +11,16 @@ router.post('/', async (req, res) => {
         where: { email: req.body.email }
     })
 
-    if (!user || !await bcrypt.compare(req.body.password, user.password_digest)) {
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
-    const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
         res.json({ user: user, token: result.value })
     }
 })
 
 router.get('/profile', async (req, res) => {
-    try {
-        const [authenticationMethod, token] = req.headers.authorization.split(' ')
-
-        if(authenticationMethod == 'Bearer') {
-            const result = await jwt.decode(process.env.JWT_SECRET, token)
-
-            const { id } = result.value
-        }
-        let user = await User.findOne({
-            where: {
-                userId: id              
-            }
-        })
-        res.json(user)
-    } catch {
-        res.json(null)
-    }
+    res.json(req.currentUser)
 })
-
 
 module.exports = router
